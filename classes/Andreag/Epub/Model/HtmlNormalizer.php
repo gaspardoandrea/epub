691ebd63840508b64d38e50d6553a307fe879c56
andreag <gaspardoandrea@gmail.com>
2020-12-07 14:30:59 +0100

    Spaces and gitignore

--- classes/Andreag/Epub/Model/HtmlNormalizer.php
+++ classes/Andreag/Epub/Model/HtmlNormalizer.php
@@ -1,20 +1,5 @@
-<?php /*
- * Copyright (C) 2020 Webformat S.r.l.
- * http://www.webformat.com
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */ /*
+<?php
+/*
  * Copyright (C) 2020 Webformat S.r.l.
  * http://www.webformat.com
  *
@@ -174,6 +159,7 @@ class HtmlNormalizer
         ];
         $tidy->parseString($html->asXML(), $config, 'utf8');
 
+        /** @noinspection PhpToStringImplementationInspection */
         $tidy = $this->postProcess((string)$tidy);
 
         file_put_contents($this->outputFile, (string)$tidy);
